2e9fd07b92dc9e06135d214e7d556d8ec6b3217a
AbdulRehmanAntall <abdulrehmanantall420@gmail.com>
2025-03-07 14:32:10 +0500

    Fixed Welcome Page message and added a color on hoovering

--- frontend/src/pages/Welcome.js
+++ frontend/src/pages/Welcome.js
@@ -18,7 +18,7 @@ function Welcome() {
                 <h2 className="welcome_h2">We Welcome You To StockSync</h2>
 
                 <p className="welcome_p">Efficient inventory management is crucial for businesses to track stock, reduce losses, and streamline operations. Without a proper system, businesses struggle with overstocking, stockouts, and mismanagement.
-                    StockWise simplifies inventory tracking, optimizes stock levels, and ensures real-time updates, so you never lose control of your inventory again!
+                    StockSync simplifies inventory tracking, optimizes stock levels, and ensures real-time updates, so you never lose control of your inventory again!
                 </p>
                 <button onClick={handleMoveForward} className="welbutton"  >Next</button>
                 <p className="welcome_p">Get started today and take charge of your inventory effortlessly!</p>
