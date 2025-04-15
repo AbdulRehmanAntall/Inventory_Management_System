7dc47f26f242dae05cc94f4475a177048af95416
kainattkk <kainat.bingo@gmail.com>
2025-04-16 01:19:53 +0500

    Added categoryController.js

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const categoryController = require('../controllers/categoryController');
 
 // Route to get all users
 router.get('/users', userController.getAllUsers); // This will now hit the getAllUsers function
 router.post('/login', userController.authenticateUser);
 router.post('/insert-user', userController.insertNewUser);
 router.post('/getUserDetailsByName', userController.getUserDetailsByName);
+router.post('/insert-category', categoryController.insertNewCategory);
 module.exports = router;
